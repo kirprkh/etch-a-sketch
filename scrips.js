5754162ccfe16799fbb2155099c2b5921d2c1265
kirillprokhorenko <kirillprokhorenko@proton.me>
2023-07-14 00:21:56 +0300

    Add function that creates grids any provided size and listens for events

--- scrips.js
+++ scrips.js
@@ -1,17 +1,30 @@
 const DEFAULT_NUMBER_OF_SQUARES_PER_SIDE = 16;
 const HOVERING_SQUARE_COLOR = 'orange';
+const SKETCH_FIELD_WIDTH = 500;
 
 const container = document.querySelector('#container');
 
-let square;
-const totalSquares = DEFAULT_NUMBER_OF_SQUARES_PER_SIDE * DEFAULT_NUMBER_OF_SQUARES_PER_SIDE;
-for (let i = 0; i < totalSquares; i++) {
-    square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+function createGrid(squaresPerSide) {
+    const totalSquares = squaresPerSide * squaresPerSide;
+    const squareSide = calculateSquareSide(squaresPerSide);
+
+    let square;
+    for (let i = 0; i < totalSquares; i++) {
+        square = document.createElement('div');
+        square.classList.add('square');
+
+        square.style.width = squareSide;
+        square.style.height = squareSide;
+
+        container.appendChild(square);
+    }
+
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = HOVERING_SQUARE_COLOR;
+    }));
 }
 
-const squares = document.querySelectorAll('.square');
-squares.forEach(square => square.addEventListener('mouseover', () => {
-    square.style.backgroundColor = HOVERING_SQUARE_COLOR;
-}));
+function calculateSquareSide(squaresPerSide) {
+    return `${SKETCH_FIELD_WIDTH / squaresPerSide}px`;
+}
