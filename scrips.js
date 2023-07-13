a72d3f1aec2af4027a84d27bafee60f40bbcaad3
kirillprokhorenko <kirillprokhorenko@proton.me>
2023-07-14 00:14:56 +0300

    Replace constant with total number of squares with number of squares per side

--- scrips.js
+++ scrips.js
@@ -1,10 +1,11 @@
-const TOTAL_SQUARES = 256;
+const DEFAULT_NUMBER_OF_SQUARES_PER_SIDE = 16;
 const HOVERING_SQUARE_COLOR = 'orange';
 
 const container = document.querySelector('#container');
 
 let square;
-for (let i = 0; i < TOTAL_SQUARES; i++) {
+const totalSquares = DEFAULT_NUMBER_OF_SQUARES_PER_SIDE * DEFAULT_NUMBER_OF_SQUARES_PER_SIDE;
+for (let i = 0; i < totalSquares; i++) {
     square = document.createElement('div');
     square.classList.add('square');
     container.appendChild(square);
